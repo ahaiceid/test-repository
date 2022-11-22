cc328825eaafd2846ac2a8c1f545831f950aced5
Aonghus Haicéid <93676994+ahaiceid@users.noreply.github.com>
2022-11-22 09:48:19 +0000

    Update scoring.py

--- scoring.py
+++ scoring.py
@@ -1,3 +1,19 @@
 class Scorer:
+    def parse(self, input):
+        '''Parse a Yahtzee input string.
+        
+        params:
+        input: a string of the form "(1, 3, 3, 2, 5) fullhouse" describing
+                a Yahtzee roll and chosen category
+
+        returns:
+        a tuple containing: a list of 5 die values, and a category string
+
+        '''
+        category = input.split()[-1]
+        dice_input = input[:input.rfind(' ')]
+        dice = dice_input[1:-1].split(', ')
+        return (dice, category)
+
     def add(self, x, y):
         return x + y
